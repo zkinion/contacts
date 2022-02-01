27d707a8277d623f657cf9260e7696d89d5a3c96
Zak <zkinion@gmail.com>
2022-01-31 21:01:33 -0800

    add swal

--- config/app.php
+++ config/app.php
@@ -189,7 +189,7 @@ return [
     */
 
     'aliases' => [
-
+        'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
