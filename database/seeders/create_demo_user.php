5306a4d4e5a24f4e0595e1a4f73052c811b67a81
Zak <zkinion@gmail.com>
2022-01-31 21:36:25 -0800

    create seeds

---
+++ database/seeders/create_demo_user.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class create_demo_user extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
